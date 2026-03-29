69f3ae63b6b462617a120634ea7c0d88f8215d7e
Parth Patel <parthpatel@macbookpro.mynetworksettings.com>
2026-03-28 23:43:33 -0700

    Removed unused boilerplate folders

--- constants/Colors.ts
+++
@@ -1,9 +0,0 @@
-export const Colors = {
-  background: '#131313',
-  surface: '#1C1B1B',
-  gold: '#E9C176',
-  blue: '#4A90E2',
-  lightblue: '#A4C9FF',
-  white: '#E5E2E1',
-  muted: '#D1C5B4',
-};
